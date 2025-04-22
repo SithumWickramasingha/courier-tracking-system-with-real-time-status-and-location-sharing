45fc2c8ab2a3c123bfe81c54cb311f1c589a0047
SithumWickramasingha <wickramasinghesithum96@gmail.com>
2025-04-23 03:15:49 +0530

    Establish connection with mysql database

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,16 +1,27 @@
 const asyncHandler = require("express-async-handler");
+const db = require("../config/dbConnection");
+
+
 //@desc Get all orders
 //@route GET /api/orders
 //@access public 
-
 const getOrders = asyncHandler(async (req,res) => {
-  res.status(200).json({message: "Get all orders"});
+
+  db.query("SELECT * FROM orders", (err, results)=>{
+    if(err){
+      console.log("DB error: ",err);
+      return res.status(500).json({message: "Failed to get orders"});
+    }
+    res.status(200).json(results);
+  })
+
+  // res.status(200).json({message: "Get all orders"});
 });
+
 
 //@desc create contacts
 //@route POST /api/contacts
 //@access public 
-
 const createOrder = asyncHandler(async (req,res) => {
   
 
@@ -23,7 +34,8 @@ const createOrder = asyncHandler(async (req,res) => {
     
   }
   console.log("The id: ", req.body);
-  res.status(200).json({message: "create a contact"});
+
+  res.status(200).json({message: `the selected id ${req.body.id}`});
 });
 
 module.exports = {getOrders, createOrder};
