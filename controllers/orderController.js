aebd11166dfb703968a7513f3e4031616ea21510
SithumWickramasingha <wickramasinghesithum96@gmail.com>
2025-04-23 02:59:41 +0530

    Use express async handler for automaticaly catches the errors and forward them to errorhandler

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,16 +1,17 @@
+const asyncHandler = require("express-async-handler");
 //@desc Get all orders
 //@route GET /api/orders
 //@access public 
 
-const getOrders = (req,res) => {
+const getOrders = asyncHandler(async (req,res) => {
   res.status(200).json({message: "Get all orders"});
-}
+});
 
 //@desc create contacts
 //@route POST /api/contacts
 //@access public 
 
-const createOrder = (req,res) => {
+const createOrder = asyncHandler(async (req,res) => {
   
 
   //handling the error
@@ -23,6 +24,6 @@ const createOrder = (req,res) => {
   }
   console.log("The id: ", req.body);
   res.status(200).json({message: "create a contact"});
-}
+});
 
 module.exports = {getOrders, createOrder};
