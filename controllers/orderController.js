1997f7a4da6a73ff0288dd62953ea9b7520f84ec
SithumWickramasingha <wickramasinghesithum96@gmail.com>
2025-04-23 13:45:39 +0530

    read database tables and retreived essential data

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -2,6 +2,9 @@ const asyncHandler = require("express-async-handler");
 const db = require("../config/dbConnection");
 
 
+var selectedItem = 0;
+let orderIds = 0;
+
 //@desc Get all orders
 //@route GET /api/orders
 //@access public 
@@ -15,8 +18,69 @@ const getOrders = asyncHandler(async (req,res) => {
     res.status(200).json(results);
   })
 
-  // res.status(200).json({message: "Get all orders"});
 });
+
+
+//@desc Get a particular order
+//@route GET /api/orders/:id
+//@access public 
+const getOrder = asyncHandler(async (req,res) =>{
+  // res.status(200).json({message: `Get a particular order ${req.params.id}`});
+
+  db.query("SELECT * FROM orders", (err, results) =>{
+    if(err){
+      console.log("DB error: ",err);
+      return res.status(500).json({message: "Failed to get orders"});
+    }
+
+    orderIds = results.map(order => order.order_id); // filter to the
+    console.log("request id: ",req.params.id);
+    
+
+    for(let i =0; i< results.length; i++){
+      if(orderIds[i] == req.params.id){
+        res.status(200).json({
+          "orderid": results[i].order_id,
+          "customer id": results[i].cutomer_id,
+          "title": results[i].title,
+          "price": results[i].price,
+          "last updated": results[i].last_updated
+        });
+
+      }else{
+        // res.status(200).json({message:"Item not found"});
+        console.log("Item not found");
+      }
+      
+    }
+  });
+
+
+  db.query("SELECT * FROM order_tracking WHERE order_id = ?", [req.params.id], (err, trackingResults) => {
+    if (err) {
+      console.log("DB error: ", err);
+      return res.status(500).json({ message: "Failed to get tracking info" });
+    }
+
+    if(trackingResults.length === 0){
+      return res.status(404).json({message: "Tracking info not found"});
+    }
+
+    const trackingInfo = {
+      tracking_id: trackingResults[0].tracking_id,
+      latitude: trackingResults[0].latitude,
+      longitude: trackingResults[0].longitude,
+      order_status: trackingResults[0].order_status,
+    };
+
+    console.log(trackingInfo);
+  });
+
+
+});
+
+
+
 
 
 //@desc create contacts
@@ -36,6 +100,8 @@ const createOrder = asyncHandler(async (req,res) => {
   console.log("The id: ", req.body);
 
   res.status(200).json({message: `the selected id ${req.body.id}`});
+  selectedItem = req.body.id;
+  console.log("Selected item: ", selectedItem);
 });
 
-module.exports = {getOrders, createOrder};
+module.exports = {getOrders, getOrder, createOrder};
--- routes/orderRouts.js
+++ routes/orderRouts.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const  {getOrders, createOrder} = require("../controllers/orderController");
+const  {getOrders,getOrder, createOrder} = require("../controllers/orderController");
 
 
 router.route("/").get(getOrders).post(createOrder);
+
+router.route("/:id").get(getOrder);
 
 module.exports = router;
